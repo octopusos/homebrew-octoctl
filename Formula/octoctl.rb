a33a84a7f29c080f4982403c41ade3c8fc210f77
Ran <ran@seacowtech.com>
2026-02-16 21:07:26 +1100

    octoctl v3.5.0

--- Formula/octoctl.rb
+++ Formula/octoctl.rb
@@ -1,23 +1,23 @@
 class Octoctl < Formula
   desc "Local manager control CLI for OctopusOS"
   homepage "https://github.com/octopusos/octoctl"
-  version "3.4.0"
+  version "3.5.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/octopusos/octoctl/releases/download/v3.4.0/octoctl-v3.4.0-darwin-arm64"
-    sha256 "b67b5d010f990583c36c72d8b8fd3e3ad63e5d32d28b372abd5f96371a17d4ac"
+    url "https://github.com/octopusos/octoctl/releases/download/v3.5.0/octoctl-v3.5.0-darwin-arm64"
+    sha256 "a4ac2a5c8f39b40978db1c392507dc9187ee3a0cf681faa5c6f161b5d45cb1bd"
 
     resource "octopus-manager" do
-      url "https://github.com/octopusos/octoctl/releases/download/v3.4.0/octopus-manager-v3.4.0-darwin-arm64"
-      sha256 "84c568879bc2786adb9749eefcee9cd369219d08e6126b425189949049625d98"
+      url "https://github.com/octopusos/octoctl/releases/download/v3.5.0/octopus-manager-v3.5.0-darwin-arm64"
+      sha256 "3792f6596a902ea20e5ae9738d14f0dbf8876247816f83b0c8170514ad05a7cd"
     end
   else
-    url "https://github.com/octopusos/octoctl/releases/download/v3.4.0/octoctl-v3.4.0-darwin-amd64"
-    sha256 "179931a64efe458ccd62ac4beca00b40b3529e55c8e4959432cc0dfdddd08b83"
+    url "https://github.com/octopusos/octoctl/releases/download/v3.5.0/octoctl-v3.5.0-darwin-amd64"
+    sha256 "6b14fc5f64f5876912818e51683f96e4a4ee46431257b2b35eb2ebae8b0e6953"
 
     resource "octopus-manager" do
-      url "https://github.com/octopusos/octoctl/releases/download/v3.4.0/octopus-manager-v3.4.0-darwin-amd64"
-      sha256 "34ee91cc941a39fd57fff00c2b30c51428143d9ed5c2450c6e62ddb68450a850"
+      url "https://github.com/octopusos/octoctl/releases/download/v3.5.0/octopus-manager-v3.5.0-darwin-amd64"
+      sha256 "7b29d51df6014b7d605714f7b0ad3af83c9bfc413433e0056aac6e23d9951ac6"
     end
   end
 
